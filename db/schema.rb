cf101a166d2a4a6a7f254893e1e998acf28baaa9
Esther Lee <estroid@gmail.com>
2016-07-13 23:37:50 -0400

    Adds table for blogs to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160710153417) do
+ActiveRecord::Schema.define(version: 20160714032004) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string   "name"
+    t.text     "excerpt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "url"
+    t.string   "image_url"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
