71a791e00759567c1b81ebd55219d0bd848aa9b5
Esther Lee <estroid@gmail.com>
2016-07-31 01:00:59 -0400

    Removes intializer for soacial-media icons

--- config/initializers/social_share_button.rb
+++
@@ -1,3 +0,0 @@
-SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook google_plus weibo qq douban google_bookmark delicious tumblr pinterest email linkedin weichat)
-end
