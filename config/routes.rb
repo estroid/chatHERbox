37cf4364bba9a42d5d99afc58b294d8713feee31
Esther Lee <estroid@gmail.com>
2016-07-31 00:03:31 -0400

    Remove social icon gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'homes/show'
-
   devise_for :users
   resources :users
   get 'welcome/index'
   root 'welcome#index'
 end
 
-Rails.application.routes.draw do
-  resources :homes, only: [:show]
-  root to: "homes#show"
-end
 
-
-# Rails.application.routes.draw do
-
-#   get 'homes/show'
-
-#   devise_for :users
-#   resources :users
-#   get 'welcome/index'
-#   root 'welcome#index'
-# end
